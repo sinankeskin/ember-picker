718baf2bbc208b31589f3b1ff55f457e6b236a47
Sinan Keskin <sinan@kesk.in>
2021-02-13 15:42:57 +0300

    Fix linting errors

--- tests/integration/modifiers/picker-test.js
+++ tests/integration/modifiers/picker-test.js
@@ -3,11 +3,11 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Modifier | picker', function(hooks) {
+module('Integration | Modifier | picker', function (hooks) {
   setupRenderingTest(hooks);
 
   // Replace this with your real tests.
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
     await render(hbs`<div {{picker}}></div>`);
 
     assert.ok(true);
